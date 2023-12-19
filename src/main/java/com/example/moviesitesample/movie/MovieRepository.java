187f85e1f087a57b1a3a1ce727d82f38649fe25e
atifkarim <atif0902082@gmail.com>
2023-12-19 23:40:05 +0100

    Add method to fetch movie by imdbId

--- src/main/java/com/example/moviesitesample/movie/MovieRepository.java
+++ src/main/java/com/example/moviesitesample/movie/MovieRepository.java
@@ -1,5 +1,7 @@
 package com.example.moviesitesample.movie;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
     
+    // Fetch movie by imdbId
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
