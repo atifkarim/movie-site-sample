48f86248e9830d60e649f09560f48d9527b3af65
atifkarim <atif0902082@gmail.com>
2023-12-19 18:41:56 +0100

    Delete the basic test
    Idea is to make a design different layer for specific purpose, eg:
    1/ Service layer
    2/ Data access layer
    3/ API layer

--- src/main/java/com/example/moviesitesample/movie/MovieApplication.java
+++ src/main/java/com/example/moviesitesample/movie/MovieApplication.java
@@ -2,22 +2,13 @@ package com.example.moviesitesample.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication
-@RestController
 public class MovieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
-	}
-
-	@GetMapping("/root")
-	public String apiRoot() {
-		return "Hello World"; 
 	}
 	
 
